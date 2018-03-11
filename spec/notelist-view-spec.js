be59ea5add4d9341dd245c4ab67924be207892fd
Serena Hathi <hathi.serena@gmail.com>
2018-03-11 13:31:26 +0000

    Each note is linked to a corresponding URL

--- spec/notelist-view-spec.js
+++ spec/notelist-view-spec.js
@@ -9,14 +9,14 @@ describe("Converting note text to html");
     var notelist = new noteList();
     notelist.addNote("Lorem ipsum dolor sit amet");
     var notelistview = new noteListView(notelist);
-    expect.toEqual(notelistview.createHtml(), "<div><li>Lorem ipsum dolor si</li></div>");
+    expect.toEqual(notelistview.createHtml(), "<div><li><a href=#notes/0>Lorem ipsum dolor si</a></li></div>");
 
   it("Handles note lists that contain multiple notes");
     var notelist2 = new noteList();
     notelist2.addNote("Excepteur sint occaecat cupidatat non proident,");
     notelist2.addNote("sunt in culpa qui officia deserunt mollit anim id est laborum.");
     var notelistview2 = new noteListView(notelist2);
-    expect.toEqual(notelistview2.createHtml(), "<div><li>Excepteur sint occae</li></div><div><li>sunt in culpa qui of</li></div>");
+    expect.toEqual(notelistview2.createHtml(), "<div><li><a href=#notes/0>Excepteur sint occae</a></li></div><div><li><a href=#notes/1>sunt in culpa qui of</a></li></div>");
 
   console.log("---")
 
--- src/notelist-view.js
+++ src/notelist-view.js
@@ -10,10 +10,13 @@
       var htmlString = ""
       var individualNotes = this.noteList.viewNotes();
       for (var i = 0; i < individualNotes.length; i++) {
-        htmlString += `<div><li>${individualNotes[i].viewText().slice(0,20)}</li></div>`;
+        htmlString += "<div><li><a href=#notes/" + i + ">" + individualNotes[i].viewText().slice(0,20) + "</a></li></div>";
       }
       return htmlString;
-    }
+    },
+    
   }
+
+
   exports.noteListView = noteListView;
 })(this);
